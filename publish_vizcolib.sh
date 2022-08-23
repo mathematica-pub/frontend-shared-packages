3764d21337b7f548f076af1c4d1c3f6324baab5f
Tom Coile <TCoile@mathematica-mpr.com>
2022-08-23 16:15:26 -0500

    test publish script

--- publish_vizcolib.sh
+++ publish_vizcolib.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 echo 'update package version'
-# npm version patch 
-# npm version patch --prefix projects/viz-components
+npm version patch --prefix projects/viz-components
+
+echo 'generate code snippets'
+cd projects/viz-components/code-snippets
+python code_snippet_generator.py 
+cd ../../..
 
 echo 'building vizcolib'
 npm install 
 ./build.sh 
+cp .vscode/vizcolib-configs.code-snippets dist/viz-components
 
 echo 'logging in to aws'
 npm run prepare --prefix projects/viz-components
 
 echo 'publishing package'
-cd dist
-npm publish @web-ast/viz-components --dry-run
+cd dist/viz-components
+npm publish
