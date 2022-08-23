ac7d8afc2cc055562a5aea2de3be8670c9a02d71
Tom Coile <TCoile@mathematica-mpr.com>
2022-08-23 13:21:36 -0500

    0.0.2

--- publish_vizcolib.sh
+++ publish_vizcolib.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+echo 'update package version'
+npm version patch 
+npm version patch --prefix projects/viz-components
+
 echo 'building vizcolib'
 npm install 
 ./build.sh 
-
-echo 'update package version'
-npm version patch 
 
 echo 'logging in to aws'
 npm run prepare --prefix projects/viz-components
 
 echo 'publishing package'
 cd dist
-npm publish @web-ast/viz-components --dry-run
+npm publish --dry-run
