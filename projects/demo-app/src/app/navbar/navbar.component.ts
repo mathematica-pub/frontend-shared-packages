9853d93cb37fd6d7f11ee093a3adb90aaf407075
Tom Coile <TCoile@mathematica-mpr.com>
2022-09-30 10:51:00 -0500

    functioning MVP navbar

--- projects/demo-app/src/app/navbar/navbar.component.ts
+++ projects/demo-app/src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { IsActiveMatchOptions, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import { parse } from 'yaml';
 
@@ -17,12 +17,6 @@ export class NavbarComponent implements OnInit {
   private http = inject(HttpClient);
   router = inject(Router);
 
-  matchOptions: IsActiveMatchOptions = {
-    paths: 'subset',
-    matrixParams: 'exact',
-    queryParams: 'ignored',
-    fragment: 'ignored',
-  };
   ngOnInit(): void {
     this.navbarConfig$ = this.http
       .get('assets/documentation/documentation-structure.yaml', {
@@ -34,27 +28,31 @@ export class NavbarComponent implements OnInit {
           const yamlObject: any = parse(text);
           const addOns = Object.assign({}, yamlObject['add-ons']);
           delete yamlObject['add-ons'];
-          delete yamlObject['library-internals'];
           for (const category in yamlObject) {
-            // if (category == 'library-internals') continue;
             for (const subcategory in yamlObject[category]) {
-              yamlObject[category][subcategory] = {
-                addOns: [],
-              };
+              if (category == 'library-internals') {
+                yamlObject[category][subcategory] = {
+                  addOns: [...Object.keys(yamlObject[category][subcategory])],
+                };
+              } else {
+                yamlObject[category][subcategory] = {
+                  addOns: [],
+                };
+              }
             }
           }
           for (const category in addOns) {
             for (const subcategory in addOns[category]) {
-              console.log(subcategory);
               if (subcategory == 'all') {
                 for (const yamlSubcategory in yamlObject[category]) {
                   const currentSubcategory =
                     yamlObject[category][yamlSubcategory];
                   yamlObject[category][yamlSubcategory].addOns = [
                     ...currentSubcategory.addOns,
-                    ...Object.keys(addOns[category][subcategory]),
+                    ...Object.keys(addOns[category][subcategory]).map(
+                      (filePath) => `all/${filePath}`
+                    ),
                   ];
-                  console.log(yamlObject[category][yamlSubcategory]);
                 }
               } else {
                 const currentSubcategory = yamlObject[category][subcategory];
@@ -68,5 +66,24 @@ export class NavbarComponent implements OnInit {
           return yamlObject;
         })
       );
+  }
+  getLink(category: string, subcategory: string, fileName: string): string {
+    if (category === 'library-internals') {
+      return `/documentation/${category}/${subcategory}/${fileName}`;
+    }
+    if (fileName.includes('all')) {
+      return `/documentation/add-ons/${category}/${fileName}`;
+    }
+    return `/documentation/add-ons/${category}/${subcategory}/${fileName}`;
+  }
+  getFileName(fileName: string): string {
+    return fileName.replace('all/', '');
+  }
+  expandLinks(category: string, subcategory: string) {
+    const url = this.router.url;
+    if (url.includes('all') && url.includes(category)) {
+      return true;
+    }
+    return url.includes(category) && url.includes(subcategory);
   }
 }
