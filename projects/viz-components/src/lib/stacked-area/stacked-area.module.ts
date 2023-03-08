cebee4628e3d25909d3b9022b0822c086cf8bf1d
Stephanie Tuerk <stuerk@mathematica-mpr.com>
2023-03-08 01:16:12 -0500

    expose stacked area events/effects to public API and export modules

--- projects/viz-components/src/lib/stacked-area/stacked-area.module.ts
+++ projects/viz-components/src/lib/stacked-area/stacked-area.module.ts
@@ -1,10 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { StackedAreaHoverAndMoveEventDirective } from './stacked-area-hover-move-event.directive';
+import { StackedAreaInputEventDirective } from './stacked-area-input-event.directive';
 import { StackedAreaComponent } from './stacked-area.component';
 
 @NgModule({
-  declarations: [StackedAreaComponent],
+  declarations: [
+    StackedAreaComponent,
+    StackedAreaHoverAndMoveEventDirective,
+    StackedAreaInputEventDirective,
+  ],
   imports: [CommonModule],
-  exports: [StackedAreaComponent],
+  exports: [
+    StackedAreaComponent,
+    StackedAreaHoverAndMoveEventDirective,
+    StackedAreaInputEventDirective,
+  ],
 })
 export class VicStackedAreaModule {}
