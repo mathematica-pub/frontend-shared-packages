c6fed7e68b51bdbeca2ab35ca21e7c2283a108df
Stephanie Tuerk <stuerk@mathematica-mpr.com>
2023-08-12 22:21:06 -0400

    update tests

--- projects/viz-components/src/lib/testing/stubs/core/overlay.stub.ts
+++ projects/viz-components/src/lib/testing/stubs/core/overlay.stub.ts
@@ -2,5 +2,6 @@ export class OverlayStub {
   create = jasmine.createSpy('create');
   scrollStrategies = {
     noop: jasmine.createSpy('noop').and.returnValue('noop' as any),
+    close: jasmine.createSpy('close').and.returnValue('close' as any),
   };
 }
