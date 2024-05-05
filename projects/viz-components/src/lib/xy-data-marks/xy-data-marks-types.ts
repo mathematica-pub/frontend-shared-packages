a87cfee43a8fa171e2fba1b56426aacf544c636b
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-05-05 01:14:17 -0400

    clean up file names

--- projects/viz-components/src/lib/xy-data-marks/xy-data-marks-types.ts
+++ projects/viz-components/src/lib/xy-data-marks/xy-data-marks-types.ts
@@ -1,26 +1,7 @@
-import { inject } from '@angular/core';
-import { Ranges } from '../chart/chart.component';
-import { VicIMarks } from '../data-marks/data-marks-types';
-import {
-  XyChartComponent,
-  XyChartScales,
-} from '../xy-chart/xy-chart.component';
-
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export class XyDataMarksValues {
   x: any[];
   y: any[];
   category: string[];
   indicies: number[];
-}
-
-export abstract class VicXyMarks implements VicIMarks {
-  chart = inject(XyChartComponent);
-  scales: XyChartScales;
-  ranges: Ranges;
-  drawMarks: () => void;
-
-  getTransitionDuration(): number {
-    return this.scales.useTransition ? this.chart.transitionDuration : 0;
-  }
 }
