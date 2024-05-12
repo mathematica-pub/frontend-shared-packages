ad59965ac79882a1cf1c86f2b00b17d79b21e0e7
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-05-12 13:03:36 -0400

    improve structure of BarsDimensions

--- projects/viz-components/src/lib/bars/bars-dimensions.ts
+++ projects/viz-components/src/lib/bars/bars-dimensions.ts
@@ -1,36 +1,26 @@
-export class VicBarsDimensions {
+export interface VicBarsDimensions {
   direction: 'vertical' | 'horizontal';
   x: 'ordinal' | 'quantitative';
   y: 'ordinal' | 'quantitative';
   ordinal: 'x' | 'y';
   quantitative: 'x' | 'y';
   quantitativeDimension: 'width' | 'height';
-
-  constructor(init?: Partial<VicBarsDimensions>) {
-    Object.assign(this, init);
-  }
 }
 
-export class VicHorizontalBarsDimensions extends VicBarsDimensions {
-  constructor() {
-    super();
-    this.direction = 'horizontal';
-    this.x = 'quantitative';
-    this.y = 'ordinal';
-    this.ordinal = 'y';
-    this.quantitative = 'x';
-    this.quantitativeDimension = 'width';
-  }
+export class VicHorizontalBarsDimensions implements VicBarsDimensions {
+  direction: 'vertical' | 'horizontal' = 'horizontal';
+  x: 'ordinal' | 'quantitative' = 'quantitative';
+  y: 'ordinal' | 'quantitative' = 'ordinal';
+  ordinal: 'x' | 'y' = 'y';
+  quantitative: 'x' | 'y' = 'x';
+  quantitativeDimension: 'width' | 'height' = 'width';
 }
 
-export class VicVerticalBarsDimensions extends VicBarsDimensions {
-  constructor() {
-    super();
-    this.direction = 'vertical';
-    this.x = 'ordinal';
-    this.y = 'quantitative';
-    this.ordinal = 'x';
-    this.quantitative = 'y';
-    this.quantitativeDimension = 'height';
-  }
+export class VicVerticalBarsDimensions implements VicBarsDimensions {
+  direction: 'vertical' | 'horizontal' = 'vertical';
+  x: 'ordinal' | 'quantitative' = 'ordinal';
+  y: 'ordinal' | 'quantitative' = 'quantitative';
+  ordinal: 'x' | 'y' = 'x';
+  quantitative: 'x' | 'y' = 'y';
+  quantitativeDimension: 'width' | 'height' = 'height';
 }
