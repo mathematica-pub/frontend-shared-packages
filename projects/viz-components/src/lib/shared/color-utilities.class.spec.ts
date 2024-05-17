a70c6605705ae23a9ad2a733cc0edda8035d1379
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-05-17 17:38:49 -0400

    replace our own color parsing method with d3 color

--- projects/viz-components/src/lib/shared/color-utilities.class.spec.ts
+++ projects/viz-components/src/lib/shared/color-utilities.class.spec.ts
@@ -16,4 +16,30 @@ describe('getContrastRatio', () => {
       ) / 100
     ).toEqual(2.76);
   });
+  it('works with rgb values', () => {
+    expect(
+      Math.floor(
+        VicColorUtilities.getContrastRatio(
+          'rgb(0, 0, 255)',
+          'rgb(255, 255, 255)'
+        ) * 100
+      ) / 100
+    ).toEqual(8.59);
+  });
+  it('works with named colors', () => {
+    expect(
+      Math.floor(VicColorUtilities.getContrastRatio('blue', 'white') * 100) /
+        100
+    ).toEqual(8.59);
+  });
+  it('works with hsl values', () => {
+    expect(
+      Math.floor(
+        VicColorUtilities.getContrastRatio(
+          'hsl(240, 100%, 50%)',
+          'hsl(0, 0%, 100%)'
+        ) * 100
+      ) / 100
+    ).toEqual(8.59);
+  });
 });
