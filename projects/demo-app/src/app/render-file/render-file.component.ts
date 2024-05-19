198d1ca6d66175f58ef2ce182d2202fc7e6b1501
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-05-19 13:56:27 -0400

    create separate overview component

--- projects/demo-app/src/app/render-file/render-file.component.ts
+++ projects/demo-app/src/app/render-file/render-file.component.ts
@@ -9,10 +9,7 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { Router } from '@angular/router';
 import { Unsubscribe } from 'projects/viz-components/src/lib/shared/unsubscribe.class';
-import { takeUntil } from 'rxjs';
-import { DocumentationService } from '../core/services/documentation.service';
 import { HighlightService } from '../core/services/highlight.service';
 
 @Component({
@@ -30,24 +27,11 @@ export class RenderFileComponent
   @Input() fileData: string;
   private highlightService = inject(HighlightService);
   private sanitizer = inject(DomSanitizer);
-  private router = inject(Router);
-  private documentationService = inject(DocumentationService);
   sanitizedDocumentation: SafeHtml;
   class: string;
 
   ngOnInit(): void {
-    if (this.router.url === '/overview') {
-      const path = 'OVERVIEW.md';
-      this.class = 'overview';
-      this.documentationService
-        .getDocumentation(path)
-        .pipe(takeUntil(this.unsubscribe))
-        .subscribe((data: string) => {
-          this.sanitizeAndHighlight(data);
-        });
-    } else {
-      this.sanitizeAndHighlight(this.fileData);
-    }
+    this.sanitizeAndHighlight(this.fileData);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
