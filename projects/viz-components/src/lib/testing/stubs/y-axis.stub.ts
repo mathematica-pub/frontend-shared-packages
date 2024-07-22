f069d74229f9e6f8ca493577c73185b5c6b8201e
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-07-21 23:14:05 -0400

    update import

--- projects/viz-components/src/lib/testing/stubs/y-axis.stub.ts
+++ projects/viz-components/src/lib/testing/stubs/y-axis.stub.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { XyAxisBase } from '../../axes/base/xy-axis-base';
+import { XyAxis } from '../../axes/base/xy-axis-base';
 import { yAxisMixin } from '../../axes/y/y-axis';
 import { DataValue } from '../../core/types/values';
 
-export class YAxisStub<T extends DataValue> extends yAxisMixin(XyAxisBase)<T> {
+export class YAxisStub<T extends DataValue> extends yAxisMixin(XyAxis)<T> {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setAxis(axisFunction: any): void {
     return;
