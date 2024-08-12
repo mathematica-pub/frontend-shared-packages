392e114658d4a9ee46056e82e7a1b595ba462f01
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-08-11 21:54:36 -0400

    update compodoc build .sh script

--- build-compodoc-documentation.sh
+++ build-compodoc-documentation.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm -rf documentation
-npm run compodoc:build -p projects/viz-components/tsconfig.doc.json -d documentation/viz-components
-pipenv run python projects/demo-app/documentation-generator/documentation-parser.py --viz-components
-npm run compodoc:build -p projects/ui-components/tsconfig.doc.json -d documentation/ui-components
+npm run compodoc:build:ui-components
 pipenv run python projects/demo-app/documentation-generator/documentation-parser.py --ui-components
+npm run compodoc:build:viz-components
+pipenv run python projects/demo-app/documentation-generator/documentation-parser.py --viz-components
