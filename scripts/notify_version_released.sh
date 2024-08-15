3da76de7928b7a315aa26a93dc730d0f23459dca
Claire McShane <CMcShane@mathematica-mpr.com>
2024-08-15 16:25:24 -0400

    feat: add release notification

--- scripts/notify_version_released.sh
+++ scripts/notify_version_released.sh
@@ -4,4 +4,4 @@ set -e
 
 version=$(node -p "require('./projects/$pkg/package.json').version")
 
-curl -X POST -H "Content-type: application/json" --data "{\"text\": \"$pkg v$version (<$pr_url|$pr_title>) has been released\"}" $SLACK_WEBHOOK_URL
+curl -X POST -H "Content-type: application/json" --data "{\"text\": \"$pkg v$version (<$pr_url|$pr_title>) has been released.\"}" $SLACK_WEBHOOK_URL
