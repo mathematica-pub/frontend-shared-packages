ffc2e0bdc816a9174174621ee755d46e073727b5
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-08-26 23:23:04 -0400

    update path for energy data in component

--- projects/my-work/src/app/content/examples/energy-intensity/energy-intensity.component.ts
+++ projects/my-work/src/app/content/examples/energy-intensity/energy-intensity.component.ts
@@ -23,7 +23,7 @@ export interface EnergyIntensityDatum {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EnergyIntensityComponent implements OnInit {
-  dataPath = '/assets/content/data/energy-intensity-data.csv';
+  dataPath = '/assets/content/example-data/energy-intensity-data.csv';
   barsData$: Observable<EnergyIntensityDatum[]>;
   linesData$: Observable<EnergyIntensityDatum[]>;
 
