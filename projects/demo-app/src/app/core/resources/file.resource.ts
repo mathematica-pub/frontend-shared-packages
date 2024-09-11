bc6ab6b6f767759e0e95de3d99f96c326de1a224
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-09-10 21:24:41 -0400

    remove unneeded as string

--- projects/demo-app/src/app/core/resources/file.resource.ts
+++ projects/demo-app/src/app/core/resources/file.resource.ts
@@ -24,6 +24,6 @@ export class FileResource {
       .get(filePath, {
         responseType: 'text',
       })
-      .pipe(map((text) => mdParse(text) as string));
+      .pipe(map((text) => mdParse(text)));
   }
 }
