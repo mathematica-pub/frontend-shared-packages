5e4d3673e9d831fbe9547205657fa6d9ef087a23
tcoile <TCoile@mathematica-mpr.com>
2024-09-12 21:02:55 -0700

    add grouped bars example

--- projects/demo-app/src/app/content/viz-components/bars-content/bars-content.component.ts
+++ projects/demo-app/src/app/content/viz-components/bars-content/bars-content.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SinglePanelExampleDisplayComponent } from '../../core/single-panel-example-display/single-panel-example-display.component';
 import { SplitPanelExampleDisplayComponent } from '../../core/split-panel-example-display/split-panel-example-display.component';
 import { BarsExampleComponent } from './bars-example/bars-example.component';
+import { GroupedBarsExampleComponent } from './grouped-bars-example/grouped-bars-example.component';
 
 @Component({
   selector: 'app-bars-content',
@@ -12,6 +13,7 @@ import { BarsExampleComponent } from './bars-example/bars-example.component';
     SinglePanelExampleDisplayComponent,
     SplitPanelExampleDisplayComponent,
     BarsExampleComponent,
+    GroupedBarsExampleComponent,
   ],
   templateUrl: './bars-content.component.html',
   styleUrls: ['../examples.scss', './bars-content.component.scss'],
