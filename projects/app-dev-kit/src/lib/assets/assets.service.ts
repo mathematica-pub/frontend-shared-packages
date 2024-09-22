6bfcccce0cb6e21c20766683576d6c0071862cd0
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-09-22 00:37:34 -0400

    rough out structure for documentation feature

---
+++ projects/app-dev-kit/src/lib/assets/assets.service.ts
@@ -0,0 +1,59 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, forkJoin } from 'rxjs';
+
+export enum AssetResponseType {
+  ArrayBuffer = 'arraybuffer',
+  Blob = 'blob',
+  Json = 'json',
+  Text = 'text',
+}
+
+@Injectable()
+export class AdkAssetsService {
+  private assets: { [key: string]: Observable<unknown> } = {};
+  private assetsPath = 'assets/';
+
+  constructor(private http: HttpClient) {}
+
+  setAssetsPath(path: string): void {
+    this.assetsPath = path;
+  }
+
+  /**
+   * Loads an asset from the assets folder.
+   *
+   * @param assetName The path to the asset from assets/. Should not include leading slash.
+   * @returns An observable that emits the asset data.
+   */
+  loadAsset(
+    assetName: string,
+    responseType: AssetResponseType
+  ): Observable<unknown> {
+    if (!this.assets[assetName]) {
+      this.assets[assetName] = this.http.get<unknown>(
+        `${this.assetsPath}${assetName}`,
+        {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          responseType: responseType as any,
+        }
+      );
+    }
+    return this.assets[assetName];
+  }
+
+  /**
+   * Loads multiple assets concurrently.
+   *
+   * @param assetNames An array of asset names to load.
+   * @returns An observable that emits an array of asset data.
+   */
+  loadAssets(
+    assetNames: string[],
+    responseType: AssetResponseType
+  ): Observable<unknown[]> {
+    return forkJoin(
+      assetNames.map((assetName) => this.loadAsset(assetName, responseType))
+    );
+  }
+}
--- projects/app-dev-kit/src/public-api.ts
+++ projects/app-dev-kit/src/public-api.ts
@@ -1,2 +1,4 @@
+export * from './lib/assets';
+export * from './lib/content-parsing';
 export * from './lib/ng-utilities';
 export const STYLES_PATH = './lib/styles/_index.scss';
