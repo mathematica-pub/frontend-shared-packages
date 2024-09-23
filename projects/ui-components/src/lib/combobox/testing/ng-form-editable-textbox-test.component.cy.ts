bf010783831eb37027c2f52ebed2811de47e3bf2
juliaWoelmer <JWoelmer@mathematica-mpr.com>
2024-09-23 16:50:31 -0400

    Added ng form editable textbox component cy tests

---
+++ projects/ui-components/src/lib/combobox/testing/ng-form-editable-textbox-test.component.cy.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import 'cypress-real-events';
+import { beforeEach, cy, describe, it } from 'local-cypress';
+import { ComboboxModule } from '../combobox.module';
+import { ComboboxService } from '../combobox.service';
+
+const scss = `
+  .combobox-textbox {
+    background: white;
+    border: 1px solid blue;
+    padding: 12px;
+  }
+`;
+
+@Component({
+  selector: 'hsi-ui-ng-form-editable-textbox-test',
+  template: `
+    <hsi-ui-ng-form-editable-textbox [inputControl]="inputControl">
+    </hsi-ui-ng-form-editable-textbox>
+    <p class="value-display">Textbook value is: {{ inputControl.value }}</p>
+  `,
+  encapsulation: ViewEncapsulation.None,
+  styles: [scss],
+})
+class NgFormEditableTextboxTestComponent {
+  inputControl = new FormControl('apples');
+}
+
+describe('NgFormListboxSingleTestComponent', () => {
+  beforeEach(() => {
+    cy.mount(NgFormEditableTextboxTestComponent, {
+      declarations: [NgFormEditableTextboxTestComponent],
+      imports: [ComboboxModule, MatIconModule],
+      providers: [ComboboxService],
+    });
+  });
+  it('displays inputControl value', () => {
+    cy.get('.combobox-textbox').should('have.value', 'apples');
+    cy.get('.value-display').should('have.text', 'Textbook value is: apples');
+  });
+  it('can change FormControl value', () => {
+    cy.get('.combobox-textbox').should('have.value', 'apples');
+    cy.get('.combobox-textbox').type('bananas');
+    cy.get('.combobox-textbox').should('have.value', 'applesbananas');
+    cy.get('.value-display').should(
+      'have.text',
+      'Textbook value is: applesbananas'
+    );
+  });
+});
