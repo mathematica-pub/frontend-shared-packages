1183bfeaa14c4d3e3eff41ec8cba14fb4eadc01d
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-09-25 19:33:33 -0400

    remove unused file, add routing for table example

--- projects/demo-app/src/app/content/routing/ui-components-routing.module.ts
+++ projects/demo-app/src/app/content/routing/ui-components-routing.module.ts
@@ -3,6 +3,13 @@ import { RouterModule } from '@angular/router';
 
 const routes = [
   {
+    path: 'table',
+    loadComponent: () =>
+      import('../ui-components/table-content/table-content.component').then(
+        (m) => m.TableContentComponent
+      ),
+  },
+  {
     path: '**',
     loadComponent: () =>
       import('../content-container/content-container.component').then(
