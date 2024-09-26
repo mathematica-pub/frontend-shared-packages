cac556415a3b12ffb5a9d6f7b57ed123aa9a114f
juliaWoelmer <JWoelmer@mathematica-mpr.com>
2024-09-26 12:38:01 -0400

    Added content to single select combobox example component

--- projects/demo-app/src/app/content/ui-components/combobox-content/single-select-combobox-example/single-select-combobox-example.component.ts
+++ projects/demo-app/src/app/content/ui-components/combobox-content/single-select-combobox-example/single-select-combobox-example.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ComboboxModule } from 'dist/ui-components';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-single-select-combobox-example',
@@ -9,5 +14,23 @@ import { ComboboxModule } from 'dist/ui-components';
   templateUrl: './single-select-combobox-example.component.html',
   styleUrl: './single-select-combobox-example.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
-export class SingleSelectComboboxExampleComponent {}
+export class SingleSelectComboboxExampleComponent {
+  options = [
+    { displayName: 'Ratatouille', id: 'rat' },
+    { displayName: 'Finding Nemo', id: 'nemo' },
+    { displayName: 'Toy Story', id: 'toy' },
+    { displayName: 'Monsters Inc.', id: 'monsterInc' },
+    { displayName: 'WALL-E', id: 'robot' },
+    { displayName: 'The Incredibles', id: 'incredibles' },
+    { displayName: 'Inside Out', id: 'insideOut' },
+    { displayName: 'Up', id: 'up' },
+  ];
+  value = new BehaviorSubject<string>(null);
+  value$ = this.value.asObservable();
+
+  onSelection(event: string): void {
+    this.value.next(event);
+  }
+}
