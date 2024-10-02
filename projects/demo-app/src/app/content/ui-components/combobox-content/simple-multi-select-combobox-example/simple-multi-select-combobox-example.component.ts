9cbf6faa935483790551b6843414214b57cf85c0
Ally Choung <AChoung@mathematica-mpr.com>
2024-10-02 14:30:22 -0400

    493 add example basic structure

--- projects/demo-app/src/app/content/ui-components/combobox-content/simple-multi-select-combobox-example/simple-multi-select-combobox-example.component.ts
+++ projects/demo-app/src/app/content/ui-components/combobox-content/simple-multi-select-combobox-example/simple-multi-select-combobox-example.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ComboboxModule } from 'dist/ui-components';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-simple-multi-select-combobox-example',
@@ -8,6 +9,21 @@ import { ComboboxModule } from 'dist/ui-components';
   imports: [CommonModule, ComboboxModule],
   templateUrl: './simple-multi-select-combobox-example.component.html',
   styleUrl: './simple-multi-select-combobox-example.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
-export class SimpleMultiSelectComboboxExampleComponent {}
+export class SimpleMultiSelectComboboxExampleComponent {
+  options = [
+    { displayName: 'Cambridge', id: 'cambridge' },
+    { displayName: 'Washington, D.C.', id: 'dc' },
+    { displayName: 'Oakland', id: 'oakland' },
+    { displayName: 'Chicago', id: 'chicago' },
+    { displayName: 'Ann Arbor', id: 'annArbor' },
+    { displayName: 'Woodlawn', id: 'woodlawn' },
+    { displayName: 'Princeton', id: 'princeton' },
+  ];
+  selected = new BehaviorSubject([]);
+  selected$ = this.selected.asObservable();
+  onSelection(selectedIds: string[]): void {
+    this.selected.next(selectedIds);
+  }
+}
