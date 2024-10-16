9f381b0479fcb2424edd3ec38909c70169c95069
juliaWoelmer <JWoelmer@mathematica-mpr.com>
2024-10-16 15:23:53 -0400

    Fixed some stuff that got messed up in merge

--- projects/demo-app/src/app/content/ui-components/combobox-content/combobox-content.component.ts
+++ projects/demo-app/src/app/content/ui-components/combobox-content/combobox-content.component.ts
@@ -2,8 +2,10 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SinglePanelExampleDisplayComponent } from '../../../platform/single-panel-example-display/single-panel-example-display.component';
 import { SplitPanelExampleDisplayComponent } from '../../../platform/split-panel-example-display/split-panel-example-display.component';
+import { ContentContainerComponent } from '../../content-container/content-container.component';
 import { DisabledSelectionsSingleExampleComponent } from './disabled-selections-single-combobox-example/disabled-selections-single-combobox-example.component';
 import { DisplaySingleSelectedComboboxExampleComponent } from './display-single-selected-combobox-example/display-single-selected-combobox-example.component';
+import { FindOnSearchSingleComboboxExampleComponent } from './find-on-search-single-combobox-example/find-on-search-single-combobox-example.component';
 import { NgFormSingleSelectComboboxExampleComponent } from './ng-form-single-select-combobox-example/ng-form-single-select-combobox-example.component';
 import { SimpleMultiSelectComboboxExampleComponent } from './simple-multi-select-combobox-example/simple-multi-select-combobox-example.component';
 import { SimpleSingleSelectComboboxExampleComponent } from './simple-single-select-combobox-example/simple-single-select-combobox-example.component';
@@ -18,8 +20,10 @@ import { SimpleSingleSelectComboboxExampleComponent } from './simple-single-sele
     SimpleSingleSelectComboboxExampleComponent,
     DisplaySingleSelectedComboboxExampleComponent,
     DisabledSelectionsSingleExampleComponent,
+    FindOnSearchSingleComboboxExampleComponent,
     NgFormSingleSelectComboboxExampleComponent,
     SimpleMultiSelectComboboxExampleComponent,
+    ContentContainerComponent,
   ],
   templateUrl: './combobox-content.component.html',
   styleUrls: ['../../examples.scss', './combobox-content.component.scss'],
