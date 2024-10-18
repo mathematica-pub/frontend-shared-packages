bd8ba2d6d0e66edc84b7b339db83e2b3c72679d4
juliaWoelmer <JWoelmer@mathematica-mpr.com>
2024-10-18 17:10:26 -0400

    Made separate directories for single and multi select examples

--- projects/demo-app/src/app/content/ui-components/combobox-content/combobox-content.component.ts
+++ projects/demo-app/src/app/content/ui-components/combobox-content/combobox-content.component.ts
@@ -3,12 +3,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SinglePanelExampleDisplayComponent } from '../../../platform/single-panel-example-display/single-panel-example-display.component';
 import { SplitPanelExampleDisplayComponent } from '../../../platform/split-panel-example-display/split-panel-example-display.component';
 import { ContentContainerComponent } from '../../content-container/content-container.component';
-import { DisabledSelectionsSingleExampleComponent } from './disabled-selections-single-combobox-example/disabled-selections-single-combobox-example.component';
-import { DisplaySingleSelectedComboboxExampleComponent } from './display-single-selected-combobox-example/display-single-selected-combobox-example.component';
-import { FindOnSearchSingleComboboxExampleComponent } from './find-on-search-single-combobox-example/find-on-search-single-combobox-example.component';
-import { NgFormSingleSelectComboboxExampleComponent } from './ng-form-single-select-combobox-example/ng-form-single-select-combobox-example.component';
-import { SimpleMultiSelectComboboxExampleComponent } from './simple-multi-select-combobox-example/simple-multi-select-combobox-example.component';
-import { SimpleSingleSelectComboboxExampleComponent } from './simple-single-select-combobox-example/simple-single-select-combobox-example.component';
+import { SimpleMultiSelectComboboxExampleComponent } from './multi-select/simple-multi-select-combobox-example/simple-multi-select-combobox-example.component';
+import { DisabledSelectionsSingleExampleComponent } from './single-select/disabled-selections-single-combobox-example/disabled-selections-single-combobox-example.component';
+import { DisplaySingleSelectedComboboxExampleComponent } from './single-select/display-single-selected-combobox-example/display-single-selected-combobox-example.component';
+import { FindOnSearchSingleComboboxExampleComponent } from './single-select/find-on-search-single-combobox-example/find-on-search-single-combobox-example.component';
+import { NgFormSingleSelectComboboxExampleComponent } from './single-select/ng-form-single-select-combobox-example/ng-form-single-select-combobox-example.component';
+import { SimpleSingleSelectComboboxExampleComponent } from './single-select/simple-single-select-combobox-example/simple-single-select-combobox-example.component';
 
 @Component({
   selector: 'app-combobox-content',
