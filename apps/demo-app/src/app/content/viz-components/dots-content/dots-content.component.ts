812aeaf260bfd64e27502cb17d87ed99d958e82e
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-11-10 16:19:22 -0500

    fix: add imports

--- apps/demo-app/src/app/content/viz-components/dots-content/dots-content.component.ts
+++ apps/demo-app/src/app/content/viz-components/dots-content/dots-content.component.ts
@@ -7,6 +7,8 @@ import {
 import { SinglePanelExampleDisplayComponent } from '../../../platform/single-panel-example-display/single-panel-example-display.component';
 import { ContentContainerComponent } from '../../content-container/content-container.component';
 import { DotsExampleComponent } from './dots-example/dots-example.component';
+import { DotsOrdinalQuantExampleComponent } from './dots-ordinal-quant-example/dots-ordinal-quant-example.component';
+import { DotsScatterplotExampleComponent } from './dots-scatterplot-example/dots-scatterplot-example.component';
 
 @Component({
   selector: 'app-dots-content',
