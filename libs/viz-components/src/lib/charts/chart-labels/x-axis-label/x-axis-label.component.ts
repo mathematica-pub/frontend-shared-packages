66de74bf4b9630e0c468aa8a055e40a13a772604
Kai <kelwooddieu@mathematica-mpr.com>
2024-12-04 14:15:00 -0500

    refactor(y axis): change to html method

--- libs/viz-components/src/lib/charts/chart-labels/x-axis-label/x-axis-label.component.ts
+++ libs/viz-components/src/lib/charts/chart-labels/x-axis-label/x-axis-label.component.ts
@@ -1,77 +1,60 @@
+import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  DestroyRef,
   ElementRef,
   Host,
   Input,
-  OnInit,
   Optional,
-  Renderer2,
 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ElementSpacing } from '../../../core';
 import { MapChartComponent } from '../../map-chart/map-chart.component';
 import { XyChartComponent } from '../../xy-chart/xy-chart.component';
 
 @Component({
   selector: 'vic-x-axis-label',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './x-axis-label.component.html',
   styleUrl: './x-axis-label.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class XAxisLabelComponent<Datum> implements OnInit {
+export class XAxisLabelComponent<Datum> {
   @Input() alignment: 'left' | 'center' = 'left';
   @Input() position: 'top' | 'bottom' = 'bottom';
   @Input() verticalOffset: number = 0;
 
   constructor(
     private el: ElementRef,
-    private renderer: Renderer2,
-    private destroyRef: DestroyRef,
     @Optional() @Host() private xyChart: XyChartComponent,
     @Optional() @Host() private mapChart: MapChartComponent<Datum>
   ) {}
 
-  ngOnInit(): void {
-    const chart = this.xyChart ? this.xyChart : this.mapChart;
-    chart.margin$
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((margin) => {
-        const titleContainer =
-          this.el.nativeElement.querySelector('.vic-x-axis-label');
+  get chart() {
+    return this.xyChart ? this.xyChart : this.mapChart;
+  }
 
-        const totalWidth = chart.width;
-        const contentWidth = totalWidth - margin.left - margin.right;
-        const chartTitleHeight =
-          chart.divRef.nativeElement.parentElement
-            .querySelector('.vic-chart-title')
-            ?.getBoundingClientRect().height || 0;
-        const titleWidth = titleContainer.offsetWidth;
-        if (this.position === 'top') {
-          this.renderer.setStyle(
-            titleContainer,
-            'top',
-            `${this.verticalOffset + chartTitleHeight}px`
-          );
-        } else if (this.position === 'bottom') {
-          this.renderer.setStyle(
-            titleContainer,
-            'bottom',
-            `${this.verticalOffset}px`
-          );
-        }
+  calculateLeft(margin: ElementSpacing): string {
+    const contentWidth = this.chart.width - margin.left - margin.right;
+    const titleWidth =
+      this.el.nativeElement.querySelector('.vic-x-axis-label').offsetWidth;
+    if (this.alignment === 'left') {
+      return `${margin.left}px`;
+    } else if (this.alignment === 'center') {
+      return `${margin.left + contentWidth / 2 - titleWidth / 2}px`;
+    }
+    return 'null';
+  }
 
-        if (this.alignment === 'left') {
-          this.renderer.setStyle(titleContainer, 'left', `${margin.left}px`);
-        } else if (this.alignment === 'center') {
-          this.renderer.setStyle(
-            titleContainer,
-            'left',
-            `${margin.left + contentWidth / 2 - titleWidth / 2}px`
-          );
-        }
-      });
+  calculateTop(): string {
+    const chartTitleHeight =
+      this.chart.divRef.nativeElement.parentElement
+        .querySelector('.vic-chart-title')
+        ?.getBoundingClientRect().height || 0;
+
+    if (this.position === 'top') {
+      return `${this.verticalOffset + chartTitleHeight}px`;
+    }
+    return 'auto';
   }
 }
--- libs/viz-components/src/lib/charts/chart-labels/y-axis-label/y-axis-label.component.ts
+++ libs/viz-components/src/lib/charts/chart-labels/y-axis-label/y-axis-label.component.ts
@@ -4,7 +4,6 @@ import {
   Component,
   Host,
   Input,
-  OnInit,
   Optional,
 } from '@angular/core';
 import { ElementSpacing } from '../../../core';
@@ -19,7 +18,7 @@ import { XyChartComponent } from '../../xy-chart/xy-chart.component';
   styleUrl: './y-axis-label.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class YAxisLabelComponent<Datum> implements OnInit {
+export class YAxisLabelComponent<Datum> {
   @Input() alignment: 'top' | 'center' = 'top';
   @Input() orientation: 'horizontal' | 'vertical' = 'vertical';
   @Input() position: 'left' | 'right' = 'left';
@@ -34,57 +33,6 @@ export class YAxisLabelComponent<Datum> implements OnInit {
   get chart() {
     return this.xyChart ? this.xyChart : this.mapChart;
   }
-
-  ngOnInit(): void {}
-
-  // ngOnInit(): void {
-  //   const chart = this.xyChart ? this.xyChart : this.mapChart;
-  //   chart.margin$
-  //     .pipe(takeUntilDestroyed(this.destroyRef))
-  //     .subscribe((margin) => {
-  //       const titleContainer =
-  //         this.el.nativeElement.querySelector('.vic-y-axis-label');
-
-  //       if (this.orientation === 'vertical') {
-  //         this.renderer.setStyle(titleContainer, 'writing-mode', 'vertical-lr');
-  //       }
-
-  //       const totalHeight = chart.height;
-  //       const chartTitleHeight =
-  //         chart.divRef.nativeElement.parentElement
-  //           .querySelector('.vic-chart-title')
-  //           ?.getBoundingClientRect().height || 0;
-  //       const contentHeight = totalHeight - margin.top - margin.bottom;
-
-  //       if (this.position === 'left') {
-  //         this.renderer.setStyle(
-  //           titleContainer,
-  //           'left',
-  //           `${this.horizontalOffset}px`
-  //         );
-  //       } else if (this.position === 'right') {
-  //         this.renderer.setStyle(
-  //           titleContainer,
-  //           'right',
-  //           `${this.horizontalOffset}px`
-  //         );
-  //       }
-
-  //       if (this.alignment === 'top') {
-  //         this.renderer.setStyle(
-  //           titleContainer,
-  //           'top',
-  //           `${margin.top + chartTitleHeight + this.verticalOffset}px`
-  //         );
-  //       } else if (this.alignment === 'center') {
-  //         this.renderer.setStyle(
-  //           titleContainer,
-  //           'top',
-  //           `${contentHeight / 2 + chartTitleHeight + this.verticalOffset}px`
-  //         );
-  //       }
-  //     });
-  // }
 
   calculateTop(margin: ElementSpacing): string {
     const chartTitleHeight =
