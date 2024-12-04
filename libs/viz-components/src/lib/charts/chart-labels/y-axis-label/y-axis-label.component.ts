70769ebca7e23fd157ef569fdcd2c88b41ce6453
Kai <kelwooddieu@mathematica-mpr.com>
2024-12-04 13:16:29 -0500

    refactor(titles): refactor to move logic into html

--- libs/viz-components/src/lib/charts/chart-labels/y-axis-label/y-axis-label.component.ts
+++ libs/viz-components/src/lib/charts/chart-labels/y-axis-label/y-axis-label.component.ts
@@ -1,22 +1,20 @@
+import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  DestroyRef,
-  ElementRef,
   Host,
   Input,
   OnInit,
   Optional,
-  Renderer2,
 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ElementSpacing } from '../../../core';
 import { MapChartComponent } from '../../map-chart/map-chart.component';
 import { XyChartComponent } from '../../xy-chart/xy-chart.component';
 
 @Component({
   selector: 'vic-y-axis-label',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './y-axis-label.component.html',
   styleUrl: './y-axis-label.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -29,60 +27,77 @@ export class YAxisLabelComponent<Datum> implements OnInit {
   @Input() horizontalOffset: number = 0;
 
   constructor(
-    private el: ElementRef,
-    private renderer: Renderer2,
-    private destroyRef: DestroyRef,
     @Optional() @Host() private xyChart: XyChartComponent,
     @Optional() @Host() private mapChart: MapChartComponent<Datum>
   ) {}
 
-  ngOnInit(): void {
-    const chart = this.xyChart ? this.xyChart : this.mapChart;
-    chart.margin$
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((margin) => {
-        const titleContainer =
-          this.el.nativeElement.querySelector('.vic-y-axis-label');
+  get chart() {
+    return this.xyChart ? this.xyChart : this.mapChart;
+  }
 
-        if (this.orientation === 'vertical') {
-          this.renderer.setStyle(titleContainer, 'writing-mode', 'vertical-lr');
-        }
+  ngOnInit(): void {}
 
-        const totalHeight = chart.height;
-        const chartTitleHeight =
-          chart.divRef.nativeElement.parentElement
-            .querySelector('.vic-chart-title')
-            ?.getBoundingClientRect().height || 0;
-        const contentHeight = totalHeight - margin.top - margin.bottom;
+  // ngOnInit(): void {
+  //   const chart = this.xyChart ? this.xyChart : this.mapChart;
+  //   chart.margin$
+  //     .pipe(takeUntilDestroyed(this.destroyRef))
+  //     .subscribe((margin) => {
+  //       const titleContainer =
+  //         this.el.nativeElement.querySelector('.vic-y-axis-label');
 
-        console.log(chartTitleHeight);
-        if (this.position === 'left') {
-          this.renderer.setStyle(
-            titleContainer,
-            'left',
-            `${this.horizontalOffset}px`
-          );
-        } else if (this.position === 'right') {
-          this.renderer.setStyle(
-            titleContainer,
-            'right',
-            `${this.horizontalOffset}px`
-          );
-        }
+  //       if (this.orientation === 'vertical') {
+  //         this.renderer.setStyle(titleContainer, 'writing-mode', 'vertical-lr');
+  //       }
 
-        if (this.alignment === 'top') {
-          this.renderer.setStyle(
-            titleContainer,
-            'top',
-            `${margin.top + chartTitleHeight + this.verticalOffset}px`
-          );
-        } else if (this.alignment === 'center') {
-          this.renderer.setStyle(
-            titleContainer,
-            'top',
-            `${contentHeight / 2 + chartTitleHeight + this.verticalOffset}px`
-          );
-        }
-      });
+  //       const totalHeight = chart.height;
+  //       const chartTitleHeight =
+  //         chart.divRef.nativeElement.parentElement
+  //           .querySelector('.vic-chart-title')
+  //           ?.getBoundingClientRect().height || 0;
+  //       const contentHeight = totalHeight - margin.top - margin.bottom;
+
+  //       if (this.position === 'left') {
+  //         this.renderer.setStyle(
+  //           titleContainer,
+  //           'left',
+  //           `${this.horizontalOffset}px`
+  //         );
+  //       } else if (this.position === 'right') {
+  //         this.renderer.setStyle(
+  //           titleContainer,
+  //           'right',
+  //           `${this.horizontalOffset}px`
+  //         );
+  //       }
+
+  //       if (this.alignment === 'top') {
+  //         this.renderer.setStyle(
+  //           titleContainer,
+  //           'top',
+  //           `${margin.top + chartTitleHeight + this.verticalOffset}px`
+  //         );
+  //       } else if (this.alignment === 'center') {
+  //         this.renderer.setStyle(
+  //           titleContainer,
+  //           'top',
+  //           `${contentHeight / 2 + chartTitleHeight + this.verticalOffset}px`
+  //         );
+  //       }
+  //     });
+  // }
+
+  calculateTop(margin: ElementSpacing): string {
+    const chartTitleHeight =
+      this.chart.divRef.nativeElement.parentElement
+        .querySelector('.vic-chart-title')
+        ?.getBoundingClientRect().height || 0;
+
+    if (this.alignment === 'top') {
+      return `${margin.top + chartTitleHeight + this.verticalOffset}px`;
+    } else if (this.alignment === 'center') {
+      const contentHeight = this.chart.height - margin.top - margin.bottom;
+      return `${contentHeight / 2 + chartTitleHeight + this.verticalOffset}px`;
+    }
+    return 'auto';
   }
 }
