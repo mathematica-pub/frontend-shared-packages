bc8566892f0a88422aed256850a79b4b7d84823f
Kai <kelwooddieu@mathematica-mpr.com>
2024-12-06 16:51:57 -0500

    refactor(x axis): add dynamic positioning

--- libs/viz-components/src/lib/charts/chart-labels/x-axis-label/x-axis-label.component.ts
+++ libs/viz-components/src/lib/charts/chart-labels/x-axis-label/x-axis-label.component.ts
@@ -5,6 +5,8 @@ import {
   ElementRef,
   Host,
   Input,
+  OnDestroy,
+  OnInit,
   Optional,
 } from '@angular/core';
 import { ElementSpacing } from '../../../core';
@@ -19,10 +21,11 @@ import { XyChartComponent } from '../../xy-chart/xy-chart.component';
   styleUrl: './x-axis-label.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class XAxisLabelComponent<Datum> {
+export class XAxisLabelComponent<Datum> implements OnInit, OnDestroy {
   @Input() alignment: 'left' | 'center' = 'left';
   @Input() position: 'top' | 'bottom' = 'bottom';
   @Input() verticalOffset: number = 0;
+  private resizeObserver: ResizeObserver;
 
   constructor(
     private el: ElementRef,
@@ -34,14 +37,37 @@ export class XAxisLabelComponent<Datum> {
     return this.xyChart ? this.xyChart : this.mapChart;
   }
 
+  ngOnInit(): void {
+    this.resizeObserver = new ResizeObserver(() => {
+      this.updatePosition();
+    });
+
+    this.resizeObserver.observe(this.chart.divRef.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect();
+    }
+  }
+
+  updatePosition(): void {
+    let margin: ElementSpacing;
+    this.chart.margin$.subscribe((value) => (margin = value));
+    const titleContainer =
+      this.el.nativeElement.querySelector('.vic-x-axis-label');
+    titleContainer.style.left = this.calculateLeft(margin);
+    titleContainer.style.top = this.calculateTop();
+  }
+
   calculateLeft(margin: ElementSpacing): string {
-    const contentWidth = this.chart.width - margin.left - margin.right;
-    const titleWidth =
-      this.el.nativeElement.querySelector('.vic-x-axis-label').offsetWidth;
+    const chartWidth = this.chart.divRef.nativeElement.clientWidth;
+    const contentWidth = chartWidth - margin.left - margin.right;
+    const titleEl = this.el.nativeElement.querySelector('.vic-x-axis-label');
     if (this.alignment === 'left') {
       return `${margin.left}px`;
     } else if (this.alignment === 'center') {
-      return `${margin.left + contentWidth / 2 - titleWidth / 2}px`;
+      return `${margin.left + contentWidth / 2 - titleEl.offsetWidth / 2}px`;
     }
     return 'null';
   }
