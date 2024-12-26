ede417916a8ee65a4871c4ebd6643a182b52a553
Kai <kelwooddieu@mathematica-mpr.com>
2024-12-26 12:06:13 -0500

    feat(chart): added chart one

---
+++ apps/my-work/src/app/content/my-work/total-expenditures/total-expenditures-bar/total-expenditures-bar.component.ts
@@ -0,0 +1,138 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import {
+  BarsConfig,
+  VicBarsConfigBuilder,
+  VicBarsModule,
+  VicChartModule,
+  VicOrdinalAxisConfig,
+  VicQuantitativeAxisConfig,
+  VicXOrdinalAxisConfigBuilder,
+  VicXQuantitativeAxisModule,
+  VicXyBackgroundModule,
+  VicXyChartModule,
+  VicYOrdinalAxisModule,
+  VicYQuantitativeAxisConfigBuilder,
+} from '@hsi/viz-components';
+import { TotalExpendituresDatum } from '../total-expenditures.component';
+
+@Component({
+  selector: 'app-expend-bar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    VicChartModule,
+    VicBarsModule,
+    VicXyChartModule,
+    VicXyBackgroundModule,
+    VicXQuantitativeAxisModule,
+    VicYOrdinalAxisModule,
+  ],
+  providers: [
+    VicBarsConfigBuilder,
+    VicYQuantitativeAxisConfigBuilder,
+    VicXOrdinalAxisConfigBuilder,
+  ],
+  templateUrl: './total-expenditures-bar.component.html',
+  styleUrls: [
+    '../total-expenditures-charts.scss',
+    './total-expenditures-bar.component.scss',
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class TotalExpendituresBarComponent implements OnInit {
+  @Input() data: TotalExpendituresDatum[];
+  sortedDataConfig: BarsConfig<TotalExpendituresDatum, string>;
+  yAxisConfig: VicQuantitativeAxisConfig<number>;
+  xAxisConfig: VicOrdinalAxisConfig<string>;
+  chartHeight = 400;
+
+  constructor(
+    private bars: VicBarsConfigBuilder<TotalExpendituresDatum, string>,
+    private yQuantitativeAxis: VicYQuantitativeAxisConfigBuilder<number>,
+    private xOrdinalAxis: VicXOrdinalAxisConfigBuilder<string>
+  ) {}
+
+  ngOnInit(): void {
+    this.setProperties();
+    console.log(this.data);
+  }
+
+  setProperties(): void {
+    const sortedData = this.data.sort((a, b) => {
+      if (a.year > b.year) return -1;
+      if (b.year > a.year) return 1;
+      else return 0;
+    });
+
+    // this.sortVarUnits = this.divideUnitsBy1000(sortedData[0].units);
+
+    this.sortedDataConfig = this.bars
+      .data(sortedData)
+      .vertical((bars) =>
+        bars
+          .x((dimension) => dimension.valueAccessor((d) => d.type))
+          .y((dimension) =>
+            dimension
+              .valueAccessor((d) => d.extreme_weather_payments)
+              .formatSpecifier(',.0f')
+              .domainPaddingPixels(16)
+          )
+      )
+      .color((dimension) => dimension.range(['#2cafb0']))
+      .labels((labels) =>
+        labels
+          .display(true)
+          .color({ default: '#2cafb0', withinBarAlternative: 'white' })
+      )
+      .getConfig();
+
+    // this.yAxisConfig = this.yOrdinalAxis.getConfig();
+    this.xAxisConfig = this.xOrdinalAxis
+      .label((label) =>
+        label
+          .text('Year Label')
+          .position('middle')
+          .anchor('middle')
+          .offset({ x: 0, y: 5 })
+      )
+      .getConfig();
+    this.yAxisConfig = this.yQuantitativeAxis
+      .tickFormat(',.0f')
+      .label((label) =>
+        label
+          .text('Millions of Dollars')
+          .position('start')
+          .anchor('start')
+          .offset({ x: 15, y: 15 })
+      )
+      .side('left')
+      .numTicks(20)
+      .getConfig();
+
+    // .label((label) =>
+    //   label
+    //     .position('middle')
+    //     .text('Percent Unemployment (US Bureau of Labor Statistics)')
+    //     .anchor('middle')
+    //     .wrap((wrap) => wrap.width(700))
+    //     .offset({ x: -15, y: 0 })
+    // )
+    // this.xAxisConfig = this.xQuantitativeAxis
+    //   .tickFormat(',.0f')
+    //   .side('bottom')
+    //   .numTicks(4)
+    //   .getConfig();
+  }
+
+  divideUnitsBy1000(units: string): string {
+    return units.replace('thousand', '').replace('million', 'thousand').trim();
+  }
+}
---
+++ apps/my-work/src/app/content/my-work/total-expenditures/total-expenditures.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { DataService } from '../../../core/services/data.service';
+import { ExportContentComponent } from '../../../platform/export-content/export-content.component';
+import { TotalExpendituresBarComponent } from './total-expenditures-bar/total-expenditures-bar.component';
+
+export interface TotalExpendituresDatum {
+  type: string;
+  year: string;
+  extreme_weather_payments: number | null;
+  extreme_weather_payments_per_beneficiary: number | null;
+}
+
+@Component({
+  selector: 'app-expend',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ExportContentComponent,
+    TotalExpendituresBarComponent,
+  ],
+  templateUrl: './total-expenditures.component.html',
+  styleUrls: ['./total-expenditures.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TotalExpendituresComponent implements OnInit {
+  dataPath = 'content/example-data/aggregated_ewp_data.csv';
+  barsData$: Observable<TotalExpendituresDatum[]>;
+  linesData$: Observable<TotalExpendituresDatum[]>;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.createData();
+  }
+
+  createData(): void {
+    const data$ = this.dataService.getDataFile(this.dataPath).pipe(
+      map((data) => {
+        const transformed: TotalExpendituresDatum[] = data.map((x) => {
+          const obj: TotalExpendituresDatum = {
+            type: x.TYPE,
+            year: x.YEAR,
+            extreme_weather_payments: parseFloat(x.EXTREME_WEATHER_PAYMENTS),
+            extreme_weather_payments_per_beneficiary:
+              x.EXTREME_WEATHER_PAYMENTS_PER_BENEFICIARY,
+          };
+          return obj;
+        });
+        return transformed;
+      })
+    );
+
+    this.barsData$ = data$.pipe(
+      map((data) => data.filter((d) => d.year === '2023'))
+    );
+  }
+}
