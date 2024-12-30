6435d5db901a4c843ba2bf43e6634c2cebe729a2
Claire McShane <CMcShane@mathematica-mpr.com>
2024-12-30 18:15:59 -0500

    ci: cherry-pick transfer over changes from testing

--- scripts/notify_version_approved.sh
+++ scripts/notify_version_approved.sh
@@ -3,10 +3,14 @@
 set -e
 set -x
 
+chmod +x ./scripts/get_slack_webhook_url.sh
+
+# Source the environment variables
+source ./scripts/set_env_vars.sh
+
 # Check if changed_pkgs is empty
 if [ -z "${changed_pkgs}" ]; then
-    echo "No packages with version bumps. Notifying maintainers Slack channel."
-    curl -X POST -H "Content-type: application/json" --data "{\"text\": \"PR (<$pr_url|$pr_title>) was approved and does not include any packages with a version bump.\"}" $SLACK_WEBHOOK_URL_MAINTAINERS
+    echo "No packages with version bumps."
     exit 0
 fi
 
@@ -17,6 +21,6 @@ for pkg in "${changed_pkgs_array[@]}"; do
     output=$(npx nx version $pkg --base=origin/main~1 --head=origin/main --dryRun --verbose 2>&1)
     new_version=$(echo "$output" | grep -oP 'Calculated new version "\K[0-9]+\.[0-9]+\.[0-9]+(?=")')
     echo "New version of $pkg: $new_version"
-    SLACK_WEBHOOK_URL=$(./get_slack_webhook_url.sh $pkg)
+    SLACK_WEBHOOK_URL=$(./scripts/get_slack_webhook_url.sh $pkg)
     curl -X POST -H "Content-type: application/json" --data "{\"text\": \"$pkg v$new_version (<$pr_url|$pr_title>) was approved and will be released on $next_weekday. Take a look if you'd like!\"}" $SLACK_WEBHOOK_URL
 done
--- scripts/notify_version_released.sh
+++ scripts/notify_version_released.sh
@@ -3,6 +3,11 @@
 set -e
 set -x
 
+chmod +x ./scripts/get_slack_webhook_url.sh
+
+# Source the environment variables
+source ./scripts/set_env_vars.sh
+
 # Check if changed_pkgs is empty
 if [ -z "${changed_pkgs}" ]; then
     echo "No packages with version bumps."
@@ -15,7 +20,6 @@ IFS=' ' read -r -a changed_pkgs_array <<< "$changed_pkgs"
 for pkg in "${changed_pkgs_array[@]}"; do
     version=$(git tag --list "$pkg-*" | sort -V | tail -n 1 | sed "s/$pkg-//")
     echo "New version of $pkg: $version"
-    SLACK_WEBHOOK_URL=$(./get_slack_webhook_url.sh $pkg)
+    SLACK_WEBHOOK_URL=$(./scripts/get_slack_webhook_url.sh $pkg)
     curl -X POST -H "Content-type: application/json" --data "{\"text\": \"$pkg v$version (<$pr_url|$pr_title>) has been released.\"}" $SLACK_WEBHOOK_URL
 done
-
