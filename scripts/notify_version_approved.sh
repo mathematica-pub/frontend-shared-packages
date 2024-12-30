1b482fa7e1cee5826b4a428cd125cdba54a3f388
Claire McShane <CMcShane@mathematica-mpr.com>
2024-12-30 18:03:00 -0500

    ci: add missing param

--- scripts/notify_version_approved.sh
+++ scripts/notify_version_approved.sh
@@ -21,6 +21,6 @@ for pkg in "${changed_pkgs_array[@]}"; do
     output=$(npx nx version $pkg --base=origin/main~1 --head=origin/main --dryRun --verbose 2>&1)
     new_version=$(echo "$output" | grep -oP 'Calculated new version "\K[0-9]+\.[0-9]+\.[0-9]+(?=")')
     echo "New version of $pkg: $new_version"
-    SLACK_WEBHOOK_URL=$(./scripts/get_slack_webhook_url.sh)
+    SLACK_WEBHOOK_URL=$(./scripts/get_slack_webhook_url.sh $pkg)
     curl -X POST -H "Content-type: application/json" --data "{\"text\": \"$pkg v$new_version (<$pr_url|$pr_title>) was approved and will be released on $next_weekday. Take a look if you'd like!\"}" $SLACK_WEBHOOK_URL
 done
