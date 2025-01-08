f1fe760e998044d6e98ea345714b66d387e31ac8
Claire McShane <CMcShane@mathematica-mpr.com>
2025-01-08 12:16:08 -0500

    ci: test

--- scripts/notify_version_approved.sh
+++ scripts/notify_version_approved.sh
@@ -3,10 +3,10 @@
 set -e
 set -x
 
-chmod +x ./scripts/get_slack_webhook_url.sh
+# chmod +x ./scripts/get_slack_webhook_url.sh
 
 # Source the environment variables
-source ./scripts/set_env_vars.sh
+# source ./scripts/set_env_vars.sh
 
 # Check if changed_pkgs is empty
 if [ -z "${changed_pkgs}" ]; then
@@ -21,6 +21,6 @@ for pkg in "${changed_pkgs_array[@]}"; do
     output=$(npx nx version $pkg --base=origin/main~1 --head=origin/main --dryRun --verbose 2>&1)
     new_version=$(echo "$output" | grep -oP 'Calculated new version "\K[0-9]+\.[0-9]+\.[0-9]+(?=")')
     echo "New version of $pkg: $new_version"
-    SLACK_WEBHOOK_URL=$(./scripts/get_slack_webhook_url.sh $pkg)
+    SLACK_WEBHOOK_URL=$(source ./scripts/get_slack_webhook_url.sh $pkg)
     curl -X POST -H "Content-type: application/json" --data "{\"text\": \"$pkg v$new_version (<$pr_url|$pr_title>) was approved and will be released on $next_weekday. Take a look if you'd like!\"}" $SLACK_WEBHOOK_URL
 done
