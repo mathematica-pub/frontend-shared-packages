425d952b0fb387a7cf0eb55f523cd5e7e7c6593a
Claire McShane <CMcShane@mathematica-mpr.com>
2025-02-03 15:28:29 -0500

    fix: don't display grid line that overlaps axis

--- libs/viz-components/src/lib/axes/base/xy-axis-base.ts
+++ libs/viz-components/src/lib/axes/base/xy-axis-base.ts
@@ -94,7 +94,9 @@ export abstract class XyAxis<TickValue extends DataValue> extends XyAuxMarks<
       .call(this.axis.tickSizeInner(this.getGridLineLength()))
       .selectAll('.tick')
       .attr('class', 'vic-grid-line')
-      .style('display', (_, i) => (this.config.grid.filter(i) ? null : 'none'))
+      .style('display', (_, i) =>
+        this.config.grid.filter(i) && i > 0 ? null : 'none'
+      )
       .select('line')
       .attr('stroke', this.config.grid.stroke.color)
       .attr('stroke-dasharray', this.config.grid.stroke.dasharray)
