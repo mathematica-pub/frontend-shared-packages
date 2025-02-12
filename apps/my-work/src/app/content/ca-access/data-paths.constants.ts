c1e20b9173d12ba1823c90df92bf01f319b22480
clindeman <clindeman@dev.ca-access.com>
2025-02-12 10:32:31 -0800

    use v2 data

--- apps/my-work/src/app/content/ca-access/data-paths.constants.ts
+++ apps/my-work/src/app/content/ca-access/data-paths.constants.ts
@@ -1,7 +1,7 @@
 const folder = 'content/data/';
 
 export const dataPath = {
-  bda: `${folder}BDA_Results.csv`,
+  bda: `${folder}BDA_Results_v2.csv`,
   csa: `${folder}Statistical_Results.csv`,
   mock: {
     bda: `${folder}Mock_BDA_Results.csv`,
