ebee07cb459c3a9a35e5ac04e7e7f53daca9e55f
Chris Lindeman <CLindeman@mathematica-mpr.com>
2025-02-14 11:18:36 -0800

    use mock data

--- apps/my-work/src/app/content/ca-access/data-paths.constants.ts
+++ apps/my-work/src/app/content/ca-access/data-paths.constants.ts
@@ -1,10 +1,14 @@
 const folder = 'content/data/';
 
-export const dataPath = {
-  bda: `${folder}BDA_Results_v2.csv`,
-  csa: `${folder}Statistical_Results.csv`,
+const path = {
+  real: {
+    bda: `${folder}BDA_Results_v2.csv`,
+    csa: `${folder}Statistical_Results.csv`,
+  },
   mock: {
     bda: `${folder}Mock_BDA_Results.csv`,
     csa: `${folder}Mock_Statistical_Results.csv`,
   },
 };
+
+export const dataPath = path.mock;
