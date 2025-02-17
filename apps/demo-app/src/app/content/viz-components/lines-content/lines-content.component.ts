c549be0d62a8a624148af13906749c6b0c7f845a
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2025-02-16 23:26:00 -0500

    docs: remove small multiples example

--- apps/demo-app/src/app/content/viz-components/lines-content/lines-content.component.ts
+++ apps/demo-app/src/app/content/viz-components/lines-content/lines-content.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SinglePanelExampleDisplayComponent } from '../../../platform/single-panel-example-display/single-panel-example-display.component';
 import { ContentContainerComponent } from '../../content-container/content-container.component';
 import { LinesExampleComponent } from './lines-example/lines-example.component';
-import { LinesSmallMultiplesExampleComponent } from './lines-small-multiples-example/lines-small-multiples-example.component';
 
 @Component({
   selector: 'app-lines-content',
@@ -13,7 +12,6 @@ import { LinesSmallMultiplesExampleComponent } from './lines-small-multiples-exa
     SinglePanelExampleDisplayComponent,
     ContentContainerComponent,
     LinesExampleComponent,
-    LinesSmallMultiplesExampleComponent,
   ],
   templateUrl: './lines-content.component.html',
   styleUrls: ['../../examples.scss', './lines-content.component.scss'],
