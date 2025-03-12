32a5515739b35ae51104e47ebf5c2d3df674e100
clindeman <clindeman@dev.ca-access.com>
2025-03-12 12:09:43 -0700

    simplify

--- apps/my-work/src/app/content/ca-access/csa/csa-dot-plot/csa-stacked-bars/csa-stacked-bars.component.ts
+++ apps/my-work/src/app/content/ca-access/csa/csa-dot-plot/csa-stacked-bars/csa-stacked-bars.component.ts
@@ -31,9 +31,9 @@ export class CsaStackedBarsComponent extends CaAccessStackedBarsComponent {
     let x = this.chart.width * 0.2;
     if (this.compPosition > 0.2 && this.compPosition < 0.66) {
       x = this.chart.width - 220;
-    } else if (this.compPosition > 0.05 && this.compPosition < 0.66) {
+    } else if (this.compPosition <= 0.2) {
       x = this.chart.width * 0.5;
-    } else if (this.compPosition > 0.05 && this.compPosition < 0.8) {
+    } else if (this.compPosition >= 0.66 && this.compPosition < 0.8) {
       x = this.chart.width * 0.14;
     }
     const percentile = this.config.data.some((d) => d.percentile25 !== null);
