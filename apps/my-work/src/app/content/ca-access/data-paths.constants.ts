b889e8edf0afc8e7f82e4a92036c64e74320095a
clindeman <clindeman@dev.ca-access.com>
2025-03-18 17:59:02 -0700

    add more old data files

--- apps/my-work/src/app/content/ca-access/data-paths.constants.ts
+++ apps/my-work/src/app/content/ca-access/data-paths.constants.ts
@@ -9,6 +9,34 @@ const path = {
     bda: `${folder}BDA_Results_v2 old charts.csv`,
     csa: `${folder}Statistical_Results old charts.csv`,
   },
+  old073024: {
+    bda: `${folder}BDA_Results_v2 old charts.csv`,
+    csa: `${folder}Statistical_Results_073024.csv`,
+  },
+  old080524: {
+    bda: `${folder}BDA_Results_v2 old charts.csv`,
+    csa: `${folder}Statistical_Results_080524.csv`,
+  },
+  old081924: {
+    bda: `${folder}BDA_Results_v2 old charts.csv`,
+    csa: `${folder}Statistical_Results_081924.csv`,
+  },
+  old082824: {
+    bda: `${folder}BDA_Results_v2 old charts.csv`,
+    csa: `${folder}Statistical_Results_082824.csv`,
+  },
+  old091924: {
+    bda: `${folder}BDA_Results_v2 old charts.csv`,
+    csa: `${folder}Statistical_Results_091924.csv`,
+  },
+  old102924: {
+    bda: `${folder}BDA_Results_v2 old charts.csv`,
+    csa: `${folder}Statistical_Results_102924.csv`,
+  },
+  old021124: {
+    bda: `${folder}BDA_Results_v2 old charts.csv`,
+    csa: `${folder}Statistical_Results_021124.csv`,
+  },
   mock: {
     bda: `${folder}Mock_BDA_Results.csv`,
     csa: `${folder}Mock_Statistical_Results.csv`,
