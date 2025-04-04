2fb5fae4579f407050bd59be096fee04f43629f2
Ally Choung <AChoung@mathematica-mpr.com>
2025-04-04 12:14:43 -0400

    fix: view encapsulation for builder method docs styling in table content

--- apps/demo-app/src/app/content/ui-components/table-content/table-content.component.ts
+++ apps/demo-app/src/app/content/ui-components/table-content/table-content.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 import { SinglePanelExampleDisplayComponent } from '../../../platform/single-panel-example-display/single-panel-example-display.component';
 import { ContentContainerComponent } from '../../content-container/content-container.component';
 import { TableExampleComponent } from './table-example/table-example.component';
@@ -14,7 +18,12 @@ import { TableExampleComponent } from './table-example/table-example.component';
     ContentContainerComponent,
   ],
   templateUrl: './table-content.component.html',
-  styleUrls: ['../../examples.scss', './table-content.component.scss'],
+  styleUrls: [
+    '../../examples.scss',
+    '../../api-documentation.scss',
+    './table-content.component.scss',
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class TableContentComponent {}
