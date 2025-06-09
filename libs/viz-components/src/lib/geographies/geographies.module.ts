cd16ef1e8e1c746a225656eea30ca36fb8f9e2bf
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2025-06-09 01:29:30 -0400

    fix: remove old geographies directives

--- libs/viz-components/src/lib/geographies/geographies.module.ts
+++ libs/viz-components/src/lib/geographies/geographies.module.ts
@@ -1,27 +1,9 @@
 import { NgModule } from '@angular/core';
-import { GeographiesClickDirective } from './events/geographies-click.directive';
 import { GeographiesEventsDirective } from './events/geographies-events.directive';
-import { GeographiesHoverMoveDirective } from './events/geographies-hover-move.directive';
-import { GeographiesHoverDirective } from './events/geographies-hover.directive';
-import { GeographiesInputEventDirective } from './events/geographies-input-event.directive';
 import { GeographiesComponent } from './geographies.component';
 
 @NgModule({
-  imports: [
-    GeographiesComponent,
-    GeographiesHoverMoveDirective,
-    GeographiesHoverDirective,
-    GeographiesInputEventDirective,
-    GeographiesClickDirective,
-    GeographiesEventsDirective,
-  ],
-  exports: [
-    GeographiesComponent,
-    GeographiesHoverMoveDirective,
-    GeographiesHoverDirective,
-    GeographiesInputEventDirective,
-    GeographiesClickDirective,
-    GeographiesEventsDirective,
-  ],
+  imports: [GeographiesComponent, GeographiesEventsDirective],
+  exports: [GeographiesComponent, GeographiesEventsDirective],
 })
 export class VicGeographiesModule {}
