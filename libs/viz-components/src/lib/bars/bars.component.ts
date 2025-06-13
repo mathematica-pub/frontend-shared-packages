bb98888b3e602e546fd026fde467c82ca1244f40
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2025-06-12 21:00:22 -0400

    fix: change style to attr to restore visibility of labels

--- libs/viz-components/src/lib/bars/bars.component.ts
+++ libs/viz-components/src/lib/bars/bars.component.ts
@@ -281,7 +281,7 @@ export class BarsComponent<
             .attr('class', this.class.label)
             .style('display', this.config.labels.display ? null : 'none')
             .text((d) => this.getLabelText(d))
-            .style('visibility', 'hidden')
+            .attr('visibility', 'hidden')
             .call((selection) => {
               selection.each((d, i, nodes) => {
                 const bbox = nodes[i].getBBox();
@@ -301,7 +301,7 @@ export class BarsComponent<
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             .transition(t as any)
             .text((d) => this.getLabelText(d))
-            .style('visiblity', 'hidden')
+            .attr('visiblity', 'hidden')
             .call((selection) => {
               selection.each((d, i, nodes) => {
                 const bbox = nodes[i].getBBox();
