39fe2e3bd385e583ef406a5020f775649ec80b17
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2025-06-12 20:45:33 -0400

    fix: typo and delay labels until in DOM

--- libs/viz-components/src/lib/bars/bars.component.ts
+++ libs/viz-components/src/lib/bars/bars.component.ts
@@ -138,7 +138,10 @@ export class BarsComponent<
       this.drawBackgrounds(transitionDuration);
     }
     if (this.config.labels) {
-      this.drawLabels(transitionDuration);
+      // must delay so that it can get the correct dimensions of the labels rather than 0 0 0 0.
+      requestAnimationFrame(() => {
+        this.drawLabels(transitionDuration);
+      });
     }
     this.updateBarElements();
   }
@@ -278,7 +281,7 @@ export class BarsComponent<
             .attr('class', this.class.label)
             .style('display', this.config.labels.display ? null : 'none')
             .text((d) => this.getLabelText(d))
-            .style('visiblity', 'hidden')
+            .style('visibility', 'hidden')
             .call((selection) => {
               selection.each((d, i, nodes) => {
                 const bbox = nodes[i].getBBox();
