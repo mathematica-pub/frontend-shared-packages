8a451799aaf7a7ff1273b4788880f32846bd99ae
Claire McShane <CMcShane@mathematica-mpr.com>
2026-05-12 12:36:03 -0400

    ci: update slack notifications

--- scripts/notify_version_approved.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-source ./scripts/set_env_vars.sh
-
-SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL_VIC
-curl -X POST -H "Content-type: application/json" --data "{\"text\": \"$pkg v$new_version (<$pr_url|$pr_title>) was approved and will be released on $next_weekday. Take a look if you'd like!\"}" $SLACK_WEBHOOK_URL
