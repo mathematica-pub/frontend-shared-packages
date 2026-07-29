877ea09b3611f70b854c1f672ef8214b94d7bef4
Chris Lindeman <CLindeman@mathematica-mpr.com>
2026-07-29 13:59:19 -0700

    remove comment

--- libs/viz/src/lib/image-download/image-download-options.ts
+++ libs/viz/src/lib/image-download/image-download-options.ts
@@ -3,11 +3,6 @@ export interface ImageDownloadOptions {
   containerNode: HTMLElement;
   fileName: string;
   filter: (domNode: HTMLElement) => boolean;
-  /**
-   * Disables external fonts from being read in
-   * Supply with custom css fallback; or set to undefined to read external fonts
-   * (will oftentimes get security errors)
-   */
   imageType: string;
   quality: number;
 }
