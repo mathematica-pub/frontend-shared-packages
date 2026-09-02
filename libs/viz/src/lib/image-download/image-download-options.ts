858d4b89187370f282a9fb649e791b264f370296
Claire McShane <54606560+cemcshane@users.noreply.github.com>
2026-09-02 16:18:23 -0400

    Revert "Upgrade image export (#794)"
    This reverts commit 7208f5f1ac2d557b3ef9190a72dd9e61f4c7a0f9.

--- libs/viz/src/lib/image-download/image-download-options.ts
+++ libs/viz/src/lib/image-download/image-download-options.ts
@@ -3,6 +3,12 @@ export interface ImageDownloadOptions {
   containerNode: HTMLElement;
   fileName: string;
   filter: (domNode: HTMLElement) => boolean;
+  /**
+   * Disables external fonts from being read in
+   * Supply with custom css fallback; or set to undefined to read external fonts
+   * (will oftentimes get security errors)
+   */
+  fontEmbedCSS: string;
   imageType: string;
   quality: number;
 }
